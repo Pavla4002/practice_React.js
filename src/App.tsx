82da676ac6868b8d575355ff34c3c673cabed705
Дмитрий Куликов <dkulikov@studio-tg.ru>
2023-06-06 10:16:32 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,44 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
-// import Profile from './components/Profile';
-// import Product from "./components/Product";
 import PurchasesList from "./components/PurchasesList";
 import FormProduct from "./components/FormProduct";
-import {IPurchases} from './types/purchases';
-import Counter from "./components/Counter";
+import { IPurchases } from './types/purchases';
 
 
-const purchases =[
-    {name:'апельсинка', count:2, cost: 15, inCart: false, id:1},
-    {name:'мандаринка', count:4, cost: 10,inCart: true,id:2},
-    {name:'огурчик', count:1, cost: 20,inCart: true,id:3},
-    {name:'поминдорка', count:1, cost: 30,inCart: true,id:4},
+const purchases = [
+  { name: 'апельсинка', count: 2, cost: 15, inCart: false, id: 1 },
+  { name: 'мандаринка', count: 4, cost: 10, inCart: true, id: 2 },
+  { name: 'огурчик', count: 1, cost: 20, inCart: true, id: 3 },
+  { name: 'поминдорка', count: 1, cost: 30, inCart: true, id: 4 },
 ];
 
 function App() {
-    const [count,setCount] =useState<number>(0);
-    const onIncrement:React.MouseEventHandler<HTMLButtonElement> = (e) =>{
-        console.log(e.target)
-        setCount(count+1);
-    }
-    const onDecrement=() =>{
-        setCount(count-1);
-    }
-    const [productList,setProduct] =useState<IPurchases[]>(purchases);
-    const addProduct= (product:IPurchases)=>{
-        product.id = Math.max(...productList.map(el => el.id),0)+1;
-            setProduct([
-                ...productList,product
-            ])
-        console.log(purchases);
-    }
+  const [productList, setProduct] = useState<IPurchases[]>(purchases);
+  const addProduct = (product: IPurchases) => {
+    product.id = Math.max(...productList.map(el => el.id), 0) + 1;
+    setProduct([
+      ...productList, product
+    ])
+    console.log(purchases);
+  }
+
+  const updateElement = (product: IPurchases) => {
+    setProduct(productList.map(p => p.id === product.id ? product : p ))
+  }
+  const deleteElement = (product: IPurchases) => {
+    setProduct(productList.filter(p => p.id !== product.id ))
+  }
 
   return (
     <div className="App">
-        <FormProduct addProduct={addProduct} />
-        <PurchasesList mas={productList} onIncrement={onIncrement} onDecrement={onDecrement} count={count}/>
+      <FormProduct addProduct={addProduct}/>
+      <PurchasesList mas={productList} updateElement={updateElement} deleteElement={deleteElement}/>
 
-        {/*<Counter count={count} onClick={onIncrement}></Counter>*/}
-        {/*<Counter count={count} onClick={onDecrement}></Counter>*/}
+      {/*<Counter count={count} onClick={onIncrement}></Counter>*/}
+      {/*<Counter count={count} onClick={onDecrement}></Counter>*/}
 
-        {/*<Profile />*/}
-        {/*<Product />*/}
+      {/*<Profile />*/}
+      {/*<Product />*/}
     </div>
   );
 }
--- src/components/PurchasesList.tsx
+++ src/components/PurchasesList.tsx
@@ -1,29 +1,26 @@
 import React from 'react';
 import '../Purchases.css'
 import Purchases from "./Purchases";
-import {useState} from "react";
-import {IPurchases} from "../types/purchases";
+import { IPurchases } from "../types/purchases";
 
 
-interface listProps{
-    mas: IPurchases[];
-    onDecrement:React.MouseEventHandler<HTMLButtonElement> | undefined;
-    onIncrement:React.MouseEventHandler<HTMLButtonElement> | undefined;
-    count:number;
-    // removeProduct:(product:IPurchases) => void
+interface listProps {
+  mas: IPurchases[];
+  updateElement: (product: IPurchases) => void;
+  deleteElement: (product: IPurchases) => void;
 }
 
-export default function PurchasesList({mas,onDecrement, onIncrement,count}: listProps){
-    const listPurchases = mas.map(element =>
-        <Purchases element={element}  onIncrement={ onIncrement} onDecrement={onDecrement} count={count} />
-    );
-    const costAll = mas.reduce((sum, el) => el.inCart ? sum+=el.cost*el.count : sum,0);
-    return(
-        <div>
-            <ul className="list">
-                {listPurchases}
-            </ul>
-            <p>Общая стоимость: { costAll} рубликов</p>
-        </div>
-    );
+export default function PurchasesList({ mas, updateElement, deleteElement }: listProps) {
+  const listPurchases = mas.map(p =>
+    <Purchases updateElement={updateElement} deleteElement={deleteElement} purchase={p} key={p.id}/>
+  );
+  const costAll = mas.reduce((sum, el) => el.inCart ? sum + el.cost * el.count : sum, 0);
+  return (
+    <div>
+      <ul className="list">
+        {listPurchases}
+      </ul>
+      <p>Общая стоимость: {costAll} рубликов</p>
+    </div>
+  );
 }
